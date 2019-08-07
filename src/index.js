879af66b4adeab78e19c81258fe7c3a4265f59a2
Tyler Hunt <tyhunt04@gmail.com>
2019-08-07 23:38:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App'
 import manageUsers from './reducers/manageUsers'
 
 
-// add imports and code
+const store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
+  <Provider>
+    <App />
+  </Provider>
   ,
   document.getElementById('root')
 );
